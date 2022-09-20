2fd362a8f8745c7df40fc709d85884266983ef6f
Paulo301 <paulovictor1918@gmail.com>
2022-09-20 16:05:35 -0300

    Configurando FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -12,6 +16,19 @@ export default NextAuth({
         params: { scope: "read:user" },
       },
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: { email } }
+        )
+      );
+
+      return true;
+    },
+  }
 });
