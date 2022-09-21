82dc7c96def03e9d07b7096fad0288f7251a30ab
Paulo301 <paulovictor1918@gmail.com>
2022-09-21 08:12:15 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,14 +21,18 @@ export default NextAuth({
     async signIn({ user, account, profile }) {
       const { email } = user;
 
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: { email } }
-        )
-      );
-
-      return true;
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        );
+  
+        return true;
+      } catch {
+        return false;
+      }
     },
   }
 });
