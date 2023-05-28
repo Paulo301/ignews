4a5a1f8fcf2eea419fbb94c01433fb4fe3280399
Paulo301 <paulovictor1918@gmail.com>
2023-05-28 11:51:45 -0300

    Página: Post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -5,6 +5,7 @@ import { createClient } from "../../../prismicio";
 import { asText } from '@prismicio/helpers';
 
 import styles from './styles.module.scss';
+import Link from "next/link";
 
 type Post = {
   slug: string; 
@@ -27,11 +28,14 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a href="" key={post.slug}>
+            <Link 
+              href={`/posts/${post.slug}`} 
+              key={post.slug}
+            >
               <time>{post.updatedAt}</time>
               <strong>{post.title}</strong>
               <p>{post.excerpt}</p>
-            </a>
+            </Link>
           ))}
         </div>
       </main>
